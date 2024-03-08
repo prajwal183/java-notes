0ace31c0bd511b857e7ba85c40653bb87c8dc3b7
Prajwal <praju.anjanappa@gmail.com>
2024-03-08 19:41:05 +0530

    scope added

---
+++ src/Scope.java
@@ -0,0 +1,10 @@
+public class Scope {
+  // class scope always use static keyword before initializing a variable in order
+  // for the variable to be accessible from anywhere in the class
+  static int a = 22;
+  public static void main(String[] args) {
+    // block scope
+    int a = 10;
+    System.out.println(a);
+  }
+}
