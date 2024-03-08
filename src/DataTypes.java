59ebf8e46c1b307bd8ece98bb4ff8d5aeef1cc0d
Prajwal <praju.anjanappa@gmail.com>
2024-03-08 19:48:04 +0530

    datatypes in java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,5 +1,27 @@
 public class DataTypes {
 
-  
-  
+  int a = 10;
+  short b = 10;
+  long c = 10;
+  byte d = 10;
+  double e = 10.5;
+  boolean f = true;
+  String g = "Hello";
+  char h = 'a';
+  float i = 10.5f;
+
+  /**
+   * byte 1 byte Stores whole numbers from -128 to 127
+   * short 2 bytes Stores whole numbers from -32,768 to 32,767
+   * int 4 bytes Stores whole numbers from -2,147,483,648 to 2,147,483,647
+   * long 8 bytes Stores whole numbers from -9,223,372,036,854,775,808 to
+   * 9,223,372,036,854,775,807
+   * float 4 bytes Stores fractional numbers. Sufficient for storing 6 to 7
+   * decimal digits
+   * double 8 bytes Stores fractional numbers. Sufficient for storing 15 decimal
+   * digits
+   * boolean 1 bit Stores true or false values
+   * char 2 bytes Stores a single character/letter or ASCII values
+   */
+
 }
